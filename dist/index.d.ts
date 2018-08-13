f3678bf53d2735b72a89f67ea7fa62cb946f0e33
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2018-08-13 17:58:31 +0300

    add prepublish

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,3 +3,4 @@ export { TestSuite } from './test-suite';
 export { GenericAssertPattern } from './assert-patterns/generic-assert-pattern';
 export { PreciseAssertPattern } from './assert-patterns/precise-assert-pattern';
 export { AbstractTestObject } from './base/abstract-test-object';
+export { DataSuite } from './base/data-suite';
