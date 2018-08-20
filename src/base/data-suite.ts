543dcecf62f88b67e1b3d38cba758ab397f896f5
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2018-08-20 16:38:38 +0300

    add extra data to DataSuite

--- src/base/data-suite.ts
+++ src/base/data-suite.ts
@@ -1,4 +1,23 @@
 export class DataSuite {
-  constructor(public name, public title, public nick?: string) {
+  datasetNick: string;
+  gitRepoOwner: string;
+
+  constructor(public name, public title) {
+  }
+
+  withDatasetNick(datasetNick: string) {
+    this.datasetNick = datasetNick;
+
+    return this;
+  }
+
+  withGitRepoOwner(gitRepoOwner: string) {
+    this.gitRepoOwner = gitRepoOwner;
+
+    return this;
+  }
+
+  getDataset() {
+    return `${this.gitRepoOwner}/${this.datasetNick}`;
   }
 }
